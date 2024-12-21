0d02c4374d6a0ebb3b9a20ed57cf9cbf1299be53
avadlamani-tech <ashwinvadlamani4@gmail.com>
2024-12-20 18:00:26 -0800

    Sign UP Screen

--- app/src/main/java/com/example/stayfinder/screens/SignIn.kt
+++ app/src/main/java/com/example/stayfinder/screens/SignIn.kt
@@ -7,9 +7,11 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Email
@@ -27,10 +29,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.stayfinder.R
 
 @Preview(showSystemUi = true, showBackground = true)
@@ -47,7 +51,7 @@ fun SignInScreen() {
 
       Row(
          Modifier.fillMaxWidth()
-            .padding(8.dp),
+            .padding(30.dp),
          verticalAlignment = Alignment.CenterVertically,
          horizontalArrangement = Arrangement.Start
 
@@ -59,7 +63,8 @@ fun SignInScreen() {
             Modifier.padding(start = 20.dp)
 
          )
-         Text("Sign in", modifier = Modifier.padding(start = 120.dp))
+         Text("Sign in", modifier = Modifier.padding(start = 120.dp),
+            fontSize = 30.sp)
 
       }
       var email by remember { mutableStateOf("") }
@@ -106,7 +111,8 @@ fun SignInScreen() {
       )
       Row(
          modifier = Modifier.padding(20.dp),
-         horizontalArrangement = Arrangement.Start
+         horizontalArrangement = Arrangement.Start,
+         verticalAlignment = Alignment.CenterVertically
 
 
 
@@ -132,7 +138,10 @@ fun SignInScreen() {
 
       }
       Button(
+         modifier = Modifier.width(250.dp)
+            .height(44.dp),
          onClick = {
+
 
          }
       ){
@@ -144,49 +153,66 @@ fun SignInScreen() {
       Spacer(
          Modifier.padding(20.dp)
       )
+      Text(
+         text = "Or continue with..."
+      )
       Row(
-         modifier = Modifier.fillMaxSize()
-            .padding(92.dp),
+         modifier = Modifier.width(300.dp)
+            .height(300.dp),
 
-         horizontalArrangement = Arrangement.SpaceEvenly
+
+
+
+         horizontalArrangement = Arrangement.SpaceEvenly,
+         verticalAlignment = Alignment.CenterVertically
 
 
       )
       {
          Image(
-            painterResource(R.drawable.img),
+            painterResource(R.drawable.googleicon),
             contentDescription = null,
             contentScale = ContentScale.Crop,
             modifier = Modifier
-               .size(55.dp)
+               .size(65.dp)
+               .clip(CircleShape)
+               .padding(10.dp)
+
 
 
 
          )
-         Spacer(
-            modifier = Modifier.padding(35.dp)
-         )
+//       Spacer(
+//            modifier = Modifier.width(10.dp)
+//         )
 
          Image(
-            painterResource(R.drawable.img),
+            painterResource(R.drawable.appleicon),
             contentDescription = null,
             contentScale = ContentScale.Crop,
             modifier = Modifier
-               .size(55.dp)
+               .size(65.dp)
+               .clip(CircleShape)
+               .padding(10.dp)
          )
-         Spacer(
-            modifier = Modifier.padding(35.dp)
-         )
-         
+//         Spacer(
+//            modifier = Modifier.width(10.dp)
+//         )
+
          Image(
-            painterResource(R.drawable.img),
+            painterResource(R.drawable.facebookicon),
             contentDescription = null,
             contentScale = ContentScale.Crop,
             modifier = Modifier
-               .size(55.dp)
+               .size(65.dp)
+               .clip(CircleShape)
+               .padding(10.dp)
          )
 
       }
+      Text(
+         text = "Don't have an account?"
+      )
 
 
    }
--- app/src/main/java/com/example/stayfinder/screens/SignUp.kt
+++ app/src/main/java/com/example/stayfinder/screens/SignUp.kt
@@ -1,2 +1,76 @@
 package com.example.stayfinder.screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Label
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.stayfinder.R
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun SignUpScreen() {
+    Row(
+        modifier = Modifier.fillMaxWidth()
+            .padding(35.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Start
+
+
+    ){
+        Icon(
+            imageVector = Icons.Default.ArrowBack,
+            contentDescription = null,
+            Modifier.padding(start = 20.dp)
+
+        )
+        Text(
+            text = "Sign Up",
+            fontSize = 15.sp,
+            modifier = Modifier.padding(start = 100.dp)
+        )
+
+
+    }
+    var email by remember { mutableStateOf("") }
+    OutlinedTextField(
+        modifier = Modifier.padding(10.dp),
+        leadingIcon ={
+            Icon(
+                imageVector = Icons.Default.Email,
+                contentDescription = null
+
+            )
+
+        },
+        value = email,
+        Label(
+            label ={
+                Text(
+
+                )
+            }
+        )
+
+
+
+    )
+
+
+}
